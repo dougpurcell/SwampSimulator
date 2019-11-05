8a30ea3880ce0a2b5bd04e7f76f4027d3e4502b1
dougpurcell <griffithsdl30@gmail.com>
2019-11-04 19:16:07 -0500

    login screen layout

--- src/swampsimulator/SwampSimulator.java
+++ src/swampsimulator/SwampSimulator.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package swampsimulator;
 
-
-
-// DOUG SUCKS
-
 /**
- *
- * @author posture
+ * @author dougthebicyclist
  */
 public class SwampSimulator {
 
@@ -20,6 +10,8 @@ public class SwampSimulator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+          Login login = new Login();   
+          login.setVisible(true);
     }
     
 }
