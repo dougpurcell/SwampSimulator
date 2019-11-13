33d8dead33a1846e45c8b07e727bba1a3c1db6d4
dougpurcell <griffithsdl30@gmail.com>
2019-11-12 21:41:56 -0500

    class today + re-writing AGAIN the effing login

--- src/swampsimulator/SwampSimulator.java
+++ src/swampsimulator/SwampSimulator.java
@@ -5,45 +5,38 @@ import javax.swing.*;
  * @author dougthebicyclist
  */
 public class SwampSimulator extends JFrame {
-    private Login loginPanel;
-    private JFrame frame;
-    
-    public void initialize() {
-        frame = new JFrame("Swamp Simulator");
-        loginPanel = new Login(this);
+
+    public SwampSimulator() {
+        super("Swamp Simulator");
+        setSize(1200,800);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+
         
-        //frame.add(startPanel);
-        frame.setSize(1200, 800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        frame.add(loginPanel);
-        frame.validate();
-        frame.pack();
-    };
+    }
 
 public static void main(String args[]) throws InterruptedException {
-      
-        SwampSimulator swampsim = new SwampSimulator();        
-        swampsim.initialize();
+        Login lgn = new Login();
+
         // TODO code application logic here
         
         // this would be the shit connected to the "checkout" button
-        JFrame frame = new JFrame("me swamp");
-        
-        Game game = new Game();
-        
-        frame.add(game);
-        frame.setSize(1280, 720);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        while (true){
-            game.move();
-            game.repaint();
-            Thread.sleep(10);
+//        JFrame frame = new JFrame("me swamp");
+//        
+//        Game game = new Game();
+//        
+//        frame.add(game);
+//        frame.setSize(1280, 720);
+//        frame.setVisible(true);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        
+//        while (true){
+//            game.move();
+//            game.repaint();
+//            Thread.sleep(10);
 
-        }
+//        }
     }
 
 }
