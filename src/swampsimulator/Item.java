0bb04090affaff1978e5feae2b77a99a7a3e3556
dougpurcell <griffithsdl30@gmail.com>
2019-11-14 09:09:02 -0500

    more adventure work during carlin's class

--- src/swampsimulator/Item.java
+++ src/swampsimulator/Item.java
@@ -5,7 +5,7 @@
  */
 package swampsimulator;
 
-import javax.swing.*; 
+import javax.swing.*;
 import java.awt.*; 
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
@@ -18,41 +18,47 @@ import javax.imageio.ImageIO;
  */
 
 class Item extends JFrame implements ActionListener{
-    private Container items;
-    protected String name;
-    protected String description;
-    protected int price;
- 
+    protected JPanel item;
+    
     public Item() {
         SwampSimulator ss = new SwampSimulator();
-//        JPanel Adventure = new JPanel();
-        //TODO Work on this too doug. come on man.
-    }
-     
-//    public class Adventure extends Item {
-//    //TODO: work on this Doug    
-//        public class ImagePanel extends JPanel{
-//            private BufferedImage thumbnail;
+        ss.setBackground(new java.awt.Color(245, 245, 245));
+        
+        int i = 1; //Fake DB
+        
+        while (i < 6) { // Fake DB
+            item = new JPanel();
+            item.setSize(200, 200);
+            item.setBackground(Color.green);
+            item.setLocation(100 * i, 50);
+            ss.add(item);
+            ++i;
+        }
+        
+        GridLayout itemgrid = new GridLayout(3,3);
+        ss.setLayout(itemgrid);
+//        
+//        GridLayout grid = new GridLayout(3,3);
+//        items.setLayout(grid);
+//        while (i < 6) {
+//            adventure = new JPanel();
+//            adventure.setSize(100, 100);
+//            adventure.setLocation(200, 30);
+//            adventure.setBackground(Color.green);
+//            items.add(adventure);
 //            
-//            public ImagePanel() {
-//                try {                
-//                thumbnail = ImageIO.read(new File("/assets/img/shrekmoviethumb.jpg"));
-//                } 
-//                catch (IOException ex) {
-//                    // handle exception...
-//                }
-//            }
-//        @Override
-//        protected void paintComponent(Graphics g) {
-//            super.paintComponent(g);
-//            g.drawImage(thumbnail, 0, 0, this); // see javadoc for more info on the parameters            
+//            i++;
 //        }
+       setVisible(true);
+    }  
+        
+        //TODO Work on this too doug. come on man.
+     
+//    public class Adventure extends Item { 
+//    //TODO: work on this Doug    
+//       
 //    }
 //    
-//    public class Character extends Item {
-//        
-//    }
-    
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
