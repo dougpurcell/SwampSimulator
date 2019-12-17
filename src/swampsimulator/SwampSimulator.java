5e4307e22a8efadbae7ca3b88d5f36faefeecd8a
dougpurcell <griffithsdl30@gmail.com>
2019-12-16 21:48:55 -0500

    update - clean with Gautam's code

--- src/swampsimulator/SwampSimulator.java
+++ src/swampsimulator/SwampSimulator.java
@@ -51,8 +51,8 @@ public class SwampSimulator {
 
 public static void main(String args[]) throws InterruptedException, IOException, LineUnavailableException, UnsupportedAudioFileException {
 
-//    SwampSimulator ss = new SwampSimulator();
-//    ss.initialize();
+    SwampSimulator ss = new SwampSimulator();
+    ss.initialize();
 
         /* C A M E R O N:
             uncomment your shit to work on the game bro.
